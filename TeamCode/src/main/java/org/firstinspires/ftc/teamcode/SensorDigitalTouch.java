48ad86652d847119c49ab36ae12fab704466cdd6
JohnMMcD <jmcdonne+github@gmail.com>
2020-08-19 15:33:09 -0400

    Removed unused DigitalChannel import.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorDigitalTouch.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorDigitalTouch.java
@@ -31,7 +31,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
 
